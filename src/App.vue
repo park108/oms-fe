1a43c4688a75a1a10a78e9a9f8f6db1e34a5802c
Jongkil Park <park108@gmail.com>
2022-03-15 22:22:26 +0900

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,5 @@
 <template>
-	<header class="header">
-		<h1 class="h1">
-			Order Management System Console
-		</h1>
-	</header>
-	<main class="main">
-		<section class="section section--split-one">
-			<Corporation @setCorpId="setCorpId" />
-		</section>
-
-		<div class="div div--dashboard-box">
-			<section class="section section--split-three">
-				<SalesOrg :corpId="corpId" />
-			</section>
-			<section class="section section--split-three">
-				<DistributionChannel :corpId="corpId" />
-			</section>
-			<section class="section section--split-three">
-				<Division :corpId="corpId" />
-			</section>
-		</div>
-
-		<section class="section section--split-one">
-			<SalesArea :corpId="corpId" />
-		</section>
-
-		<div class="div div--dashboard-box">
-			<section class="section section--split-two">
-				<SalesOffice :corpId="corpId" />
-			</section>
-			<section class="section section--split-two">
-				<SalesGroup :corpId="corpId" />
-			</section>
-		</div>
-	</main>
-	<footer class="footer">
-		© 2022 Jongkil Park.
-	</footer>
+	<router-view/>
 </template>
 
-<script src="./App.js"></script>
-
-<style>
-	@import url(./App.css);
-</style>
+<style src="./App.css" />
---
+++ src/views/HomeView.vue
@@ -0,0 +1,72 @@
+<template>
+	<header class="header">
+		<h1 class="h1">
+			Order Management System Console
+		</h1>
+	</header>
+	<main class="main">
+		<section class="section section--split-one">
+			<Corporation @setCorpId="setCorpId" />
+		</section>
+
+		<div class="div div--dashboard-box">
+			<section class="section section--split-three">
+				<SalesOrg :corpId="corpId" />
+			</section>
+			<section class="section section--split-three">
+				<DistributionChannel :corpId="corpId" />
+			</section>
+			<section class="section section--split-three">
+				<Division :corpId="corpId" />
+			</section>
+		</div>
+
+		<section class="section section--split-one">
+			<SalesArea :corpId="corpId" />
+		</section>
+
+		<div class="div div--dashboard-box">
+			<section class="section section--split-two">
+				<SalesOffice :corpId="corpId" />
+			</section>
+			<section class="section section--split-two">
+				<SalesGroup :corpId="corpId" />
+			</section>
+		</div>
+	</main>
+	<footer class="footer">
+		© 2022 Jongkil Park.
+	</footer>
+</template>
+
+<script>
+import Corporation from "../components/Corporation.vue";
+import SalesOrg from "../components/SalesOrg.vue";
+import DistributionChannel from "../components/DistributionChannel.vue";
+import Division from "../components/Division.vue";
+import SalesArea from "../components/SalesArea.vue";
+import SalesOffice from "../components/SalesOffice.vue";
+import SalesGroup from "../components/SalesGroup.vue";
+
+export default {
+	data() {
+		return {
+			corpId: String
+		}
+	},
+	components: {
+		Corporation,
+		SalesOrg,
+		DistributionChannel,
+		Division,
+		SalesArea,
+		SalesOffice,
+		SalesGroup,
+	},
+	methods: {
+		setCorpId(value) {
+			this.corpId = value;
+		}
+	},
+};
+</script>
