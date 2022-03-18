4ee9cf20a00c7ba4b27b77d33fee8bc4bb83078f
Jongkil Park <park108@gmail.com>
2022-03-18 22:11:31 +0900

    Separate vue and js at index page

---
+++ src/views/Index.js
@@ -0,0 +1,30 @@
+
+import Corporation from "../components/Corporation.vue";
+import SalesOrg from "../components/SalesOrg.vue";
+import DistributionChannel from "../components/DistributionChannel.vue";
+import Division from "../components/Division.vue";
+import SalesArea from "../components/SalesArea.vue";
+import SalesOffice from "../components/SalesOffice.vue";
+import SalesGroup from "../components/SalesGroup.vue";
+
+export default {
+	data() {
+		return {
+			corpId: String
+		}
+	},
+	components: {
+		Corporation,
+		SalesOrg,
+		DistributionChannel,
+		Division,
+		SalesArea,
+		SalesOffice,
+		SalesGroup,
+	},
+	methods: {
+		setCorpId(value) {
+			this.corpId = value;
+		}
+	},
+};
