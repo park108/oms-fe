586de5d5ac3eaefc23b8a250a7efa5064b75d758
Jongkil Park <park108@gmail.com>
2022-03-19 20:35:08 +0900

    Add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from '../views/Index.vue'
+import SalesOrg from '../views/SalesOrg.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Index',
-    component: Index
-  }
+	{ path: '/', name: 'Index', component: Index },
+	{ path: '/orgs/:org', name: 'SalesOrg', component: SalesOrg },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes
 })
 
 export default router
---
+++ src/views/SalesOrg.vue
@@ -0,0 +1,7 @@
+<template>
+	<header class="header">
+		<h1 class="h1">
+			Sales Organization
+		</h1>
+	</header>
+</template>
