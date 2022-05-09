f2d87b5bb41eca41784730db23e8d2949593c389
Jongkil Park <park108@gmail.com>
2022-05-09 11:21:32 +0900

    Add test case in customer list

--- src/customers/CustomerDataHandler.js
+++ src/customers/CustomerDataHandler.js
@@ -45,48 +45,112 @@ export class CustomerDataHandler {
 
 const dummyData = [
 	{
-		customerNo: "210987",
+		customerNo: "1000359",
+		customerName: "Mitsubishi Fuso Truck&Bus Corporation",
+		address: "10 Okura-Cho, Nakahara-Kukawasaki, 2118522, Japan",
+		salesAreaData: [
+			{
+				salesOrg: "S000",
+				distributionChannel: "40",
+				division: "51",
+				salesOffice: "S100",
+				salesGroup: "100",
+				currency: "USD",
+				customerPricingProcedure: "1",
+				deliveringPlant: "",
+				shippingCondition: "01",
+				incoterms: "DDU",
+				paymentTerms: "G120",
+				creditControlArea: "S110"
+			},
+		]
+	},
+	{
+		customerNo: "1000376",
+		customerName: "Solarwatt Gmbh",
+		address: "Maria-Reiche-Str, 2AD-01109, Dresden, 01109, Germany",
+		salesAreaData: [
+			{
+				salesOrg: "S000",
+				distributionChannel: "40",
+				division: "51",
+				salesOffice: "S100",
+				salesGroup: "100",
+				currency: "EUR",
+				customerPricingProcedure: "1",
+				deliveringPlant: "",
+				shippingCondition: "01",
+				incoterms: "EXW",
+				paymentTerms: "YT01",
+				creditControlArea: "S110"
+			},
+		]
+	},
+	{
+		customerNo: "1000383",
 		customerName: "Daimler AG",
 		address: "Mercedesstrasse 120, Stuttgart, 70372, Germany",
 		salesAreaData: [
 			{
 				salesOrg: "S000",
-				distributionChannel: "20",
-				division: "51",
-			},
-			{
-				salesOrg: "S000",
 				distributionChannel: "40",
 				division: "51",
+				salesOffice: "S100",
+				salesGroup: "100",
+				currency: "EUR",
+				customerPricingProcedure: "1",
+				deliveringPlant: "",
+				shippingCondition: "01",
+				incoterms: "DAP",
+				paymentTerms: "C045",
+				creditControlArea: "S110"
 			},
 		]
 	},
 	{
-		customerNo: "210028",
-		customerName: "Volkswagen AG",
+		customerNo: "1001983",
+		customerName: "Volkswagen Aktiengesellschaft",
 		address: "Berliner Ring 2, 38440 Wolfsburg, Germany",
 		salesAreaData: [
 			{
 				salesOrg: "S000",
 				distributionChannel: "40",
 				division: "51",
+				salesOffice: "S100",
+				salesGroup: "100",
+				currency: "EUR",
+				customerPricingProcedure: "1",
+				deliveringPlant: "",
+				shippingCondition: "01",
+				incoterms: "DDU",
+				paymentTerms: "C030",
+				creditControlArea: "S110"
 			},
 		]
 	},
 	{
-		customerNo: "210029",
-		customerName: "Volkswagen America",
-		address: "2200 Woodland Pointe Avenue Herndon, VA 20171 United States",
+		customerNo: "1002148",
+		customerName: "Valmet Automotive Inc",
+		address: "23501 Uusikaupunki, 23501, Finland",
 		salesAreaData: [
 			{
 				salesOrg: "S000",
 				distributionChannel: "40",
 				division: "51",
+				salesOffice: "S100",
+				salesGroup: "100",
+				currency: "EUR",
+				customerPricingProcedure: "1",
+				deliveringPlant: "",
+				shippingCondition: "01",
+				incoterms: "DAF",
+				paymentTerms: "C045",
+				creditControlArea: "S110"
 			},
 		]
 	},
 	{
-		customerNo: "248361",
+		customerNo: "1000396",
 		customerName: "Ford Motor Company",
 		address: "1 American Rd, Dearborn, MI 48126, United States",
 		salesAreaData: [
@@ -94,35 +158,76 @@ const dummyData = [
 				salesOrg: "S000",
 				distributionChannel: "40",
 				division: "51",
+				salesOffice: "S100",
+				salesGroup: "100",
+				currency: "USD",
+				customerPricingProcedure: "1",
+				deliveringPlant: "",
+				shippingCondition: "01",
+				incoterms: "FCA",
+				paymentTerms: "C120",
+				creditControlArea: "S110"
 			},
 		]
 	},
 	{
-		customerNo: "200163",
+		customerNo: "1073324",
 		customerName: "Polestar",
 		address: "Assar Gabrielssons Väg 9. SE-405 31 Gothenburg. Sweden",
-		salesAreaData: [
-			{
-				salesOrg: "S000",
-				distributionChannel: "40",
-				division: "51",
-			},
-		]
 	},
 	{
 		customerNo: "1018116406",
-		customerName: "Hyundai Mobis",
+		customerName: "Hyundai Mobis Co.,Ltd.",
 		address: "203, Teheran-ro Gangnam-gu Seoul, 06141, South Korea",
 		salesAreaData: [
 			{
 				salesOrg: "S000",
 				distributionChannel: "20",
 				division: "51",
+				salesOffice: "S100",
+				salesGroup: "100",
+				currency: "KRW",
+				customerPricingProcedure: "1",
+				deliveringPlant: "1752",
+				shippingCondition: "01",
+				incoterms: "DDP",
+				paymentTerms: "G090",
+				creditControlArea: "S100"
 			},
 			{
 				salesOrg: "S000",
-				distributionChannel: "40",
+				distributionChannel: "20",
 				division: "51",
+				salesOffice: "S100",
+				salesGroup: "100",
+				currency: "KRW",
+				customerPricingProcedure: "1",
+				deliveringPlant: "1752",
+				shippingCondition: "01",
+				incoterms: "DDP",
+				paymentTerms: "C135",
+				creditControlArea: "S100"
+			},
+		]
+	},
+	{
+		customerNo: "3128179335",
+		customerName: "SK Mobile Enegy Co.,Ltd.",
+		address: "207, Seongyeon-ro, Seongyeon-myeon, 31930, South Korea",
+		salesAreaData: [
+			{
+				salesOrg: "S000",
+				distributionChannel: "20",
+				division: "51",
+				salesOffice: "S100",
+				salesGroup: "100",
+				currency: "KRW",
+				customerPricingProcedure: "1",
+				deliveringPlant: "",
+				shippingCondition: "01",
+				incoterms: "EXW",
+				paymentTerms: "C060",
+				creditControlArea: "S100"
 			},
 		]
 	},
